72f544a0d45c9951bec5213292fd635d104c2d02
rapt0p7 <rapt0p7@gmail.com>
2017-03-03 21:56:34 +0300

    Добавил смену цветов хедера

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,19 @@ var hamburger = document.querySelector(".hamburger-menu");
 var menu = document.querySelector(".menu");
 var ham_icon = document.querySelector(".hamburger-menu__icon--ham");
 var cross = document.querySelector(".hamburger-menu__icon--cross");
+var header = document.querySelector(".page-header");
 
 hamburger.addEventListener("click", function(event) {
 	event.preventDefault();
 	if (menu.classList.contains("menu--open")) {
 		menu.classList.remove("menu--open");
 		ham_icon.classList.remove("hamburger-menu__icon--hidden");
+		header.classList.remove("page-header--toggled");
 		cross.classList.add("hamburger-menu__icon--hidden");
 		} else {
 			menu.classList.add("menu--open");
 			cross.classList.remove("hamburger-menu__icon--hidden");
 			ham_icon.classList.add("hamburger-menu__icon--hidden");
+			header.classList.add("page-header--toggled");
 		}
 });
